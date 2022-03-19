51251c2c92d4abf9ea6bacb1dd4261475d78f1df
amazingcoderNadir <95256318+amazingcoderNadir@users.noreply.github.com>
2022-03-19 19:40:24 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBDJzzmvii9IFo-1rIjxeaJMitEfcWD1kM",
+    authDomain: "kwitter-54629.firebaseapp.com",
+    databaseURL: "https://kwitter-54629-default-rtdb.firebaseio.com",
+    projectId: "kwitter-54629",
+    storageBucket: "kwitter-54629.appspot.com",
+    messagingSenderId: "65688062679",
+    appId: "1:65688062679:web:b0282902c8084a6f317e92"
+  };  
