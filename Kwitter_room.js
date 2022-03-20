bf16db04c094ecb52ec587c21d1aba4bdbe35759
amazingcoderNadir <95256318+amazingcoderNadir@users.noreply.github.com>
2022-03-20 18:28:35 -0400

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -7,3 +7,36 @@ var firebaseConfig = {
     messagingSenderId: "65688062679",
     appId: "1:65688062679:web:b0282902c8084a6f317e92"
   };  
+
+    function addRoom()
+    {
+          room_name = document.getElementById("room_name").value;
+
+          firebase.database().ref("/").child(room_name).update({
+                purpose: "adding room name"
+          });
+          localStorage.setItem("room_name", room_name);
+
+          window.location = "kwitter_page.html";
+    }
+
+    function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+
+//Start code
+console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML+= row;
+//End code
+});});}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+
+}
